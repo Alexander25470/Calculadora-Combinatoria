8f3f44c38743b75c744322077736cb65947c3bc8
Alexander25470 <61051951+Alexander25470@users.noreply.github.com>
2020-08-04 17:36:36 -0300

    Add files via upload

--- js/formulas.js
+++ js/formulas.js
@@ -4,20 +4,19 @@ function factorial (n) {
 		total *= i; 
 	}
 	return total; 
-}
+};
 function calcularVariacion(n,r){
 var variacionFactorial=factorial(n-r);
 var nFactorial=factorial(n);
 var resultado=nFactorial/variacionFactorial;
-var resultadomostrar=document.getElementById("resultado");
-resultadomostrar.textContent="El resultado es: "+resultado;
 
-}
+document.getElementById("resultado").textContent="El resultado es: "+resultado;
+};
 function calcularCombinacion(n,r){
     var variacionFactorial=factorial(n-r);
     var nFactorial=factorial(n);
     var rFactorial=factorial(r);
     var resultado=nFactorial/(rFactorial*variacionFactorial);
-    var resultadomostrar=document.getElementById("resultado");
-    resultadomostrar.textContent="El resultado es: "+resultado;
-}
+
+    document.getElementById("resultado").textContent="El resultado es: "+resultado;
+};
